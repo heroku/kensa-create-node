68e74f499a27db9fbd9cf8cf65350fb7637b73ed
Chris Continanza <christopher.continanza@gmail.com>
2011-11-19 03:00:07 -0800

    planchange and destroy

--- web.js
+++ web.js
@@ -2,6 +2,24 @@ var express = require('express');
 
 var resources = [];
 
+function get_resource(id) {
+ id = parseInt(id)
+ for (i in resources) {
+   if(resources[i].id == id){
+     return resources[i] 
+   }
+ }
+}
+
+function destroy_resource(id) {
+ id = parseInt(id)
+ for (i in resources) {
+   if(resources[i].id == id){
+     delete resources[i] 
+   }
+ }
+}
+
 function basic_auth (req, res, next) {
     if (req.headers.authorization && req.headers.authorization.search('Basic ') === 0) {
         // fetch login and password
@@ -25,20 +43,30 @@ function basic_auth (req, res, next) {
 
 var app = express.createServer(express.logger());
 
-app.configure(function(){
-  app.use(express.bodyParser());
-})
-
 app.get('/', function(request, response) {
     response.send('Hello World!');
 });
 
-app.post('/heroku/resources', basic_auth, function(request, response) {
+app.post('/heroku/resources', express.bodyParser(), basic_auth, function(request, response) {
   console.log(request.body)
   var resource =  {id : resources.length + 1, plan : request.body.plan }
   resources.push(resource)
   response.send(resource)
 });
+
+app.put('/heroku/resources/:id', express.bodyParser(), basic_auth, function(request, response) {
+  console.log(request.body)
+  console.log(request.params) 
+  var resource =  get_resource(request.params.id)
+  resource.plan = request.body.plan
+  response.send("ok")
+})
+
+app.delete('/heroku/resources/:id', basic_auth, function(request, response) {
+  console.log(request.params)
+  destroy_resource(request.params.id)
+  response.send("ok")
+})
 
 var port = process.env.PORT || 4567;
 app.listen(port, function() {
